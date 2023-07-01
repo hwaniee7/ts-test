1f29328146dc224699eac4ab8fe0a9b381d7a5af
hwaniee413 <hwaniee413@gmail.com>
2023-07-01 21:15:26 +0900

    sweetalert2 이벤트

--- src/InterfaceTest3.js
+++ src/InterfaceTest3.js
@@ -1,18 +1,91 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+var react_1 = __importDefault(require("react"));
+var reactstrap_1 = require("reactstrap");
+var sweetalert2_1 = __importDefault(require("sweetalert2"));
 var Bmw = /** @class */ (function () {
     function Bmw(color, wheels) {
         this.color = color;
         this.wheels = wheels;
     }
     Bmw.prototype.start = function () {
-        console.log(" is running.");
+        //console.log(" is running.");
+        Sweetalert_start();
     };
     return Bmw;
 }());
+var Sweetalert_stop = function () {
+    sweetalert2_1.default.fire({
+        text: 'stop!!',
+        icon: 'warning',
+        showConfirmButton: false,
+        timer: 2000
+    });
+};
+var Sweetalert_start = function () {
+    sweetalert2_1.default.fire({
+        text: 'run!!',
+        icon: 'success',
+        showConfirmButton: false,
+        timer: 2000
+    });
+};
 var InterfaceTest3 = function () {
-    var car = new Bmw("Blue", 4);
-    console.log("name: " + car.constructor.name + ", color: " + car.color + ", wheels: " + car.wheels);
-    car.start();
+    var bmw = new Bmw("Blue", 4);
+    var benz = {
+        name: "Benz",
+        door: 5,
+        stop: function () {
+            //console.log("stop");
+            Sweetalert_stop();
+        },
+        color: "black",
+        wheels: 4,
+        start: function () {
+            //console.log(" is running.");
+            Sweetalert_start();
+        }
+    };
+    var handleBmwStart = function () {
+        bmw.start();
+    };
+    var handleBenzStart = function () {
+        benz.start();
+    };
+    var handleBenzStop = function () {
+        benz.stop();
+    };
+    return (react_1.default.createElement(react_1.default.Fragment, null,
+        react_1.default.createElement(reactstrap_1.Table, null,
+            react_1.default.createElement("thead", null,
+                react_1.default.createElement("tr", null,
+                    react_1.default.createElement("td", null, "Name"),
+                    react_1.default.createElement("td", null, "Color"),
+                    react_1.default.createElement("td", null, "Door"),
+                    react_1.default.createElement("td", null, "Wheels"),
+                    react_1.default.createElement("td", null, "Start"),
+                    react_1.default.createElement("td", null, "Stop"))),
+            react_1.default.createElement("tbody", null,
+                react_1.default.createElement("tr", null,
+                    react_1.default.createElement("td", null, bmw.constructor.name),
+                    react_1.default.createElement("td", null, bmw.color),
+                    react_1.default.createElement("td", null),
+                    react_1.default.createElement("td", null, bmw.wheels),
+                    react_1.default.createElement("td", null,
+                        react_1.default.createElement(reactstrap_1.Button, { color: "success", onClick: handleBmwStart }, "Start")),
+                    react_1.default.createElement("td", null)),
+                react_1.default.createElement("tr", null,
+                    react_1.default.createElement("td", null, benz.name),
+                    react_1.default.createElement("td", null, benz.color),
+                    react_1.default.createElement("td", null, benz.door),
+                    react_1.default.createElement("td", null, benz.wheels),
+                    react_1.default.createElement("td", null,
+                        react_1.default.createElement(reactstrap_1.Button, { color: "success", onClick: handleBenzStart }, "Start")),
+                    react_1.default.createElement("td", null,
+                        react_1.default.createElement(reactstrap_1.Button, { color: "warning", onClick: handleBenzStop }, "Stop")))),
+            react_1.default.createElement("tfoot", null))));
 };
 exports.default = InterfaceTest3;
--- src/InterfaceTest3.tsx
+++ src/InterfaceTest3.tsx
@@ -1,9 +1,16 @@
-
+import React from "react";
+import { Table, Button } from "reactstrap";
+import Swal from "sweetalert2";
 
 interface Car {
     color:string,
     wheels:number,
     start(): void,
+}
+interface Benz extends Car {
+    name?:string,
+    door:number,
+    stop():void,
 }
 
 class Bmw implements Car {
@@ -14,14 +21,102 @@ class Bmw implements Car {
         this.wheels=wheels;
     }
     start() {
-        console.log(" is running.");
+        //console.log(" is running.");
+        Sweetalert_start();
     }
 
 }
-const InterfaceTest3 = () =>{
-    const car = new Bmw("Blue", 4);
-    console.log("name: " + car.constructor.name + ", color: " + car.color + ", wheels: " + car.wheels);
-    car.start()
+const Sweetalert_stop = () =>{
+    Swal.fire({
+        text: 'stop!!',
+        icon: 'warning',
+        showConfirmButton: false,
+        timer:2000
+    })
+}  
+const Sweetalert_start = () => {
+    Swal.fire({
+        text: 'run!!',
+        icon: 'success',
+        showConfirmButton: false,
+        timer:2000
+    })
+}
+const InterfaceTest3:React.FC = () =>{
+    const bmw = new Bmw("Blue", 4);
+    const benz : Benz = {
+        name: "Benz",
+        door : 5,
+        stop() {
+            //console.log("stop");
+            Sweetalert_stop();
+           
+        },
+        color : "black",
+        wheels : 4,
+        start() {
+            //console.log(" is running.");
+            Sweetalert_start();
+        }   
+    }
+
+    const handleBmwStart = () => {
+        bmw.start();
+    }
+    const handleBenzStart = () => {
+        benz.start();
+    }
+    const handleBenzStop = () => {
+        benz.stop();
+    }
+
+    
+    return (
+        <>
+            <Table>
+                <thead>
+                    <tr>
+                        <td>Name</td>
+                        <td>Color</td>
+                        <td>Door</td>
+                        <td>Wheels</td>
+                        <td>Start</td>
+                        <td>Stop</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>{bmw.constructor.name}</td>
+                        <td>{bmw.color}</td>
+                        <td></td>
+                        <td>{bmw.wheels}</td>
+                        <td>
+                            <Button color="success" onClick={handleBmwStart}>Start</Button>
+                        </td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>{benz.name}</td>
+                        <td>{benz.color}</td>
+                        <td>{benz.door}</td>
+                        <td>{benz.wheels}</td>
+                        <td>
+                    
+                            <Button color="success" onClick={handleBenzStart}>Start</Button>
+                        </td>
+                        <td>
+                            <Button color="warning" onClick={handleBenzStop}>Stop</Button>
+                        </td>
+                    </tr>
+                </tbody>
+                <tfoot>
+
+                </tfoot>
+
+            </Table>
+        </>
+
+    )
 }
 
 export default InterfaceTest3;
